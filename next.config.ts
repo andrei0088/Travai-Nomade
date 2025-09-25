2f6cb0984250ea8f9f72ef074b9e13acde88cf01
Andrei D2u <andreidavidoiu@gmail.com>
2025-09-25 19:39:32 +0200

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Activează exportul static
+  output: "export",
+
+  // Alte configurări opționale
+  reactStrictMode: true,
+  // dacă folosești imagini, ai nevoie să setezi un loader compatibil cu exportul static
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
